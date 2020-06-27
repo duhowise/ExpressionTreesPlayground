0325e57d3aa054e0688fcd91b3f15f9420697442
Wise Duho <duhowise@gmail.com>
2020-06-27 22:47:58 +0000

    Dispatch func execution to another method

--- src/ExpressionTreesPlayground/Program.cs
+++ src/ExpressionTreesPlayground/Program.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace ExpressionTreesPlayground {
     class Program {
         static void Main (string[] args) {
-            Func<MyClass, string> func = c => c.MyMethod (1, "");
-            Expression<Func<MyClass, string>> expression = c => c.MyMethod (1, "");
+            Func<MyClass, string> func = c => c.MyMethod (1, "some data");
+          //Dispatch func execution to another method
+            AnotherMethod(func);
+           var stringOutput= func(new MyClass());
+        }
+
+        private static void AnotherMethod(Func<MyClass, string> someFunc)
+        {
+            //do something with method
         }
     }
 }
